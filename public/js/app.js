a6b24e8c752fc160d20696707c0614dcb31e60d3
hamza-133 <hamzabouelwahab04@gmail.com>
2026-01-11 17:44:43 +0100

    feat : add logic button prevouis

--- public/js/app.js
+++ public/js/app.js
@@ -1,28 +1,33 @@
-let carrouselImg =Array.from(document.querySelectorAll(".carrousel-img img"))
-let prevBtn = document.querySelector("#prev")
-let nextBtn = document.querySelector("#next")
+let carrouselImg = Array.from(document.querySelectorAll(".carrousel-img img"));
+let prevBtn = document.querySelector("#prev");
+let nextBtn = document.querySelector("#next");
 
-let carruntIndex = 0
+let carruntIndex = 0;
 
-nextBtn.onclick = nextSlide
-prevBtn.onclick = prevSlide
+prevBtn.onclick = prevSlide;
+nextBtn.onclick = nextSlide;
 
-function showSlide (index) {
-    carrouselImg.forEach(img => {
+function showSlide(index) {
+  carrouselImg.forEach((img) => {
+    img.classList.remove("active");
+  });
 
-        img.classList.remove("active")
-
-    })
-
-    carrouselImg[index].classList.add("active")
+  carrouselImg[index].classList.add("active");
 }
 
 function nextSlide() {
-    carruntIndex--
-    if(carruntIndex < 0){
-        carruntIndex = carrouselImg.length -1
-    }
-    showSlide(carruntIndex)
-    
+  carruntIndex++;
+
+  if (carruntIndex >= carrouselImg.length) {
+    carruntIndex = 0;
+  }
+  showSlide(carruntIndex);
 }
 
+function prevSlide() {
+  carruntIndex--;
+  if (carruntIndex < 0) {
+    carruntIndex = carrouselImg.length - 1;
+  }
+  showSlide(carruntIndex);
+}
